37bc22d27432f5eda8cf0226843583da10bf2698
Evan Lewis <ehlewis@me.com>
2020-02-10 22:44:06 -0500

    Update gedcom_parser.py
    makes a table, or text file, but not both

--- gedcom_parser.py
+++ gedcom_parser.py
@@ -14,26 +14,7 @@ import csv
 from prettytable import PrettyTable
 #create a dictionary that stores the values of each of the tags
 
-dict = {
-
-        'NOTE':'0',
-        'HEAD':'0',
-        'TRLR':'0',
-        'FAM':'0',
-        'INDI':'0',
-        'NAME':'1',
-        'SEX':'1',
-        'BIRT':'1',
-        'DEAT':'1',
-        'HUSB':'1',
-        'WIFE':'1',
-        'MARR':'1',
-        'DIV':'1',
-        'CHIL':'1',
-        'FAMC':'1',
-        'FAMS':'1',
-        'DATE':'2'
-        }
+dict = {'NOTE':'0', 'HEAD':'0', 'TRLR':'0', 'FAM':'0', 'INDI':'0', 'NAME':'1', 'SEX':'1', 'BIRT':'1', 'DEAT':'1', 'HUSB':'1', 'WIFE':'1', 'MARR':'1', 'DIV':'1', 'CHIL':'1', 'FAMC':'1', 'FAMS':'1', 'DATE':'2'}
 
 fname = input("Enter file name: ")
 print(fname)
@@ -42,50 +23,35 @@ workFile = open(fname)
 output = open('parsed_output.txt', 'w+')
 output_lines = []
 
-#check to see if the file is empty
-#if so then exit. if not then continue parsing
-if os.stat(fname).st_size == 0 :
-    print("Empty file")
-    exit()
-else :
-    individual_table = PrettyTable()
-    individual_table.field_names = ["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"]
 
-    family_table = PrettyTable()
-    family_table.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"]
+individual_table = PrettyTable()
+individual_table.field_names = ["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"]
 
-    workFile.seek(0)
-    for line in workFile :
+family_table = PrettyTable()
+family_table.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"]
 
-        line = line.rstrip()
-        #print out the current line
-        output.write('--> ' + line + '\n')
-        output_lines.append('--> ' + line + '\n')
+
+def validation(line):
+
         #splitting up the line into appropriate
         info = line.split()
         level = info[0]
         #getting the keys
         keys = dict.keys()
 
-
-
         #going through each line
-        for curr_key in keys :
+        for curr_key in keys:
             #checking to see if the key is in the dictionary
             if curr_key in line and dict[curr_key] == level and curr_key in info :
                 #extra checking if the key is indi or fam
                 if (curr_key == 'INDI' or curr_key == 'FAM') :
-
                     #checking validity of each tag on its line
-                    if line.endswith(curr_key) :
+                    if line.endswith(curr_key):
                         validity = 'Y'
                         tag = curr_key
                         args = info[1]
 
-                        print(line)
-                        #Family table
-
-                        break
+                        return [validity, level, tag, args]
 
                     else :
                         validity = 'N'
@@ -93,25 +59,102 @@ else :
                         #selecting where to split each line
                         #to seperate the rest of the arguements
                         args = line.split(tag, 1)[1].lstrip()
-                        break
+                        return [validity, level, tag, args]
 
                 validity = 'Y'
                 tag = curr_key
                 args = line.split(tag, 1)[1].lstrip()
-                print(line)
                 # Valid
+                return [validity, level, tag, args]
 
-                break
+        # Not valid tag
+        validity = 'N'
+        tag = info[1]
+        args = ''
+        for x in info[2:] :
+            args += x + ' '
+        return [validity, level, tag, args]
 
-            # Not valid tag
-            else :
-                validity = 'N'
-                tag = info[1]
-                args = ''
-                for x in info[2:] :
-                    args += x + ' '
 
-        output.write('<-- ' + level + '|' + tag + '|' + validity + '|' + args + '\n')
-        output_lines.append('<-- ' + level + '|' + tag + '|' + validity + '|' + args + '\n')
 
-#print(output_lines)
+def parse_to_text(workFile):
+    workFile.seek(0)
+    for line in workFile:
+        line = line.rstrip()
+        #print out the current line
+        output.write('--> ' + line + '\n')
+        output_lines.append('--> ' + line + '\n')
+        outVal = validation(line)
+        print(outVal)
+        curr_line = '<-- ' + outVal[1] + '|' + outVal[2] + '|' + outVal[0] + '|' + outVal[3] + '\n'
+        output.write(curr_line)
+        output_lines.append(curr_line)
+
+
+def validate_to_array(workFile):
+    outList = []
+    for line in workFile:
+        line = line.rstrip()
+        outList.append(validation(line))
+    return outList
+
+def parse_to_chart(workFile):
+    indiObj = {"ID":"", "Name":"", "Gender":"", "Birthday":"", "Age":"", "Alive":"", "Death":"", "Child":"", "Spouse":""}
+    famObj = {"ID":"", "Married":"", "Divorced":"", "Husband ID":"", "Husband Name":"", "Wife ID":"", "Wife Name":"", "Children":""}
+    outList = validate_to_array(workFile)
+    currEntry = 0
+    while currEntry < len(outList):
+        if outList[currEntry][1] == "0":
+            # if individual
+            if outList[currEntry][2] == "INDI":
+                while outList[currEntry+1][1] != "0":
+                    if outList[currEntry][2] == "INDI":
+                        indiObj["ID"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "NAME":
+                        indiObj["Name"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "SEX":
+                        indiObj["Gender"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "BIRT":
+                        indiObj["Birthday"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "DEAT":
+                        indiObj["Death"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "CHIL":
+                        indiObj["Child"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "HUSB":
+                        indiObj["Spouse"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "WIFE":
+                        indiObj["Spouse"] = outList[currEntry][3]
+
+                    currEntry += 1
+
+                individual_table.add_row([indiObj["ID"],indiObj["Name"],indiObj["Gender"],indiObj["Birthday"],indiObj["Age"],indiObj["Alive"],indiObj["Death"],indiObj["Child"],indiObj["Spouse"]])
+
+            #If fam
+            if outList[currEntry][2] == "FAM":
+                while outList[currEntry+1][1] != "0":
+                    if outList[currEntry][2] == "FAM":
+                        famObj["ID"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "MARR":
+                        famObj["Married"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "DIV":
+                        famObj["Divorced"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "HUSB":
+                        famObj["Husband ID"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "HUSB":
+                        famObj["Husband Name"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "WIFE":
+                        famObj["Wife ID"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "WIFE":
+                        famObj["Wife Name"] = outList[currEntry][3]
+                    if outList[currEntry][2] == "CHIL":
+                        famObj["Children"] = outList[currEntry][3]
+
+                    currEntry += 1
+                family_table.add_row([famObj["ID"],famObj["Married"],famObj["Divorced"],famObj["Husband ID"],famObj["Husband Name"],famObj["Wife ID"],famObj["Wife Name"],famObj["Children"]])
+        currEntry += 1
+
+#print(validate_to_array(workFile))
+#print(parse_to_text(workFile))
+print(parse_to_chart(workFile))
+print(individual_table)
+print(family_table)
