c714011295f2cc97f8fb8a0eeb5486c3b104a85b
hrana2 <32397923+hrana2@users.noreply.github.com>
2020-02-26 17:46:33 -0500

    us06 and us07 test functions

---
+++ good_parser.py
@@ -0,0 +1,152 @@
+from prettytable import PrettyTable
+from datetime import datetime,date
+import pickle
+
+x= PrettyTable()
+y= PrettyTable()
+lines=[]
+outputlines=[]
+with open('Rana_test.ged') as line:
+    lines=line.read().splitlines()
+
+tagdictionary={
+    "INDI":"0",
+    "NAME":"1",
+    "SEX":"1",
+    "BIRT":"1",
+    "DEAT":"1",
+    "FAMC":"1",
+    "FAMS":"1",
+    "FAM":"0",
+    "MARR":"1",
+    "HUSB":"1",
+    "WIFE":"1",
+    "CHIL":"1",
+    "DIV":"1",
+    "DATE":"2",
+    "HEAD":"0",
+    "TRLR":"0",
+    "NOTE":"0"
+
+}
+for i in lines:
+    outputlines.append("--> "+i)
+    word=i.split(" ")
+
+    if(word[1] in tagdictionary.keys()):
+        if(int(word[0])==int(tagdictionary[word[1]]) and word[1] not in ["INDI","FAM"]):
+            if(len(word)>2):
+                outputlines.append("<-- "+word[0]+"|"+word[1]+"|"+"Y"+"|"+" ".join(word[2:]))
+            else:
+                outputlines.append("<-- "+word[0]+"|"+word[1]+"|"+"Y"+"|")
+        else:
+            if(len(word)>2):
+                outputlines.append("<-- "+word[0]+"|"+word[1]+"|"+"N"+"|"+" ".join(word[2:]))
+            else:
+                outputlines.append("<-- "+word[0]+"|"+word[1]+"|"+"N"+"|")
+    elif( len(word)>2 and (word[2] in tagdictionary.keys()) ):
+        if(int(word[0])==int(tagdictionary[word[2]])):
+            outputlines.append("<-- "+word[0]+"|"+word[2]+"|"+"Y"+"|"+word[1])
+        else:
+            outputlines.append("<-- "+word[0]+"|"+word[2]+"|"+"N"+"|"+word[1])
+    else:
+        if(len(word)>2):
+            outputlines.append("<-- "+word[0]+"|"+word[1]+"|"+"N"+"|"+" ".join(word[2:]))
+        else:
+            outputlines.append("<-- "+word[0]+"|"+word[1]+"|"+"N"+"|")
+
+#Getting all valid lines from the input
+validlines=[]
+for i in outputlines:
+    if ('Y' in i.split("|")):
+        validlines.append(i)
+
+validlinesstring="\n".join(validlines)
+validpeople=[]
+#Splitting all the individual records 
+for i in validlinesstring.split("INDI"):
+    validpeople.append(i)
+#Getting  all the family records 
+families=validpeople[-1].split("0|FAM")[1:]
+validpeople[-1]=validpeople[-1].split("0|FAM")[0]
+#Getting and fetching all the details in Individuals Table
+x.field_names = ["ID", "Name", "Gender", "Birthday","Alive","Age","Death","Child","Spouse"]
+a = set()
+b = set()
+person=['N/A','N/A','N/A','N/A',True,'N/A','N/A','N/A','N/A']
+
+for i in range(1, len(validpeople)):
+    individual=validpeople[i].split("\n")
+    a = set()
+    b = set()
+    person=['N/A','N/A','N/A','N/A',True,'N/A','N/A','N/A','N/A']
+    for j in range(0,len(individual)):
+        
+        if individual[j].split("|")[0]=='':
+            person[0]=((individual[j].split("|")[-1]).replace('@',''))
+        elif len(individual[j].split("|")) >1 and individual[j].split("|")[1]=='NAME':
+            person[1]=(individual[j].split("|")[-1])
+        elif len(individual[j].split("|")) >1 and individual[j].split("|")[1]=='SEX':
+            person[2]=(individual[j].split("|")[-1])
+        elif len(individual[j].split("|")) >1 and individual[j].split("|")[1]=='BIRT':
+            person[3]=(individual[j+1].split("|")[-1])
+            born = datetime.strptime((individual[j+1].split("|")[-1]), '%d %b %Y')
+            today=date.today()
+            person[5]=(today.year - born.year - ((today.month, today.day) < (born.month, born.day)))
+        elif len(individual[j].split("|")) >1 and individual[j].split("|")[1]=='DEAT':
+            person[4]=False
+            person[6]=(individual[j+1].split("|")[-1])
+            death=datetime.strptime((individual[j+1].split("|")[-1]), '%d %b %Y')
+            person[5]=(death.year - born.year - ((death.month, death.day) < (born.month, born.day)))
+        elif len(individual[j].split("|")) >1 and individual[j].split("|")[1]=='FAMS':
+            b.add((individual[j].split("|")[-1]).replace('@',''))
+            person[8]=b
+        elif len(individual[j].split("|")) >1 and individual[j].split("|")[1]=='FAMC':
+            a.add((individual[j].split("|")[-1]).replace('@',''))
+            person[7]=a
+        
+    x.add_row(person)
+#Getting and Fetching all the details in Families Table
+y.field_names = ["ID", "Married", "Divorced", 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
+c = set()
+person1 = ['N/A','N/A','N/A','N/A','N/A','N/A','N/A','N/A']
+
+for i in range(0, len(families)):
+    member = families[i].split("\n") 
+    c = set()
+    person1 = ['N/A','N/A','N/A','N/A','N/A','N/A','N/A','N/A'] 
+    for j in range(0,len(member)): 
+        mb = member[j].split('|')
+        if '' == mb[0]: #Getting all the ids 
+            last = mb[-1].replace('@','') 
+            person1[0] = last
+        elif 'HUSB' in mb: #Getting all the Huband IDS and their respected name
+            last = mb[-1].replace('@','')
+            person1[3] = last
+            for row in x:
+                row.border = False
+                row.header = False
+                if (row.get_string(fields=["ID"]).strip()) == last:
+                    person1[4]=(row.get_string(fields=["Name"]).strip())
+        elif 'WIFE' in mb: #Getting all the Huband IDS and their respected name
+            last = mb[-1].replace('@','')
+            person1[5] = last
+            for row in x:
+                row.border = False
+                row.header = False
+                if (row.get_string(fields=["ID"]).strip()) == last:
+                    person1[6]=(row.get_string(fields=["Name"]).strip())
+        elif 'CHIL' in mb: #Getting all the Children IDS and storing it in variable of type set
+            last = mb[-1].replace('@','')
+            c.add(last)
+            person1[7] = c
+        elif 'DIV' in mb: #Getting information about divorce details
+            person1[2] = member[j+1].split('|')[-1]
+        elif 'MARR' in mb: #Getting information about Marriage details
+            person1[1] = member[j+1].split('|')[-1]
+    y.add_row(person1)
+
+print ("Individuals")
+print (x)
+print ("Families")
+print (y)
