bbb7678d93389bad8af1c0a556e9d10ca7b43db3
hrana2 <32397923+hrana2@users.noreply.github.com>
2020-04-13 00:04:52 -0400

    fixed errors

--- tests.py
+++ tests.py
@@ -21,7 +21,7 @@ class TestUM(unittest.TestCase):
         self.assertEqual(test_us05_marr_b4_death(), "Error: Family: @F2@: US05: Death of husband before married 8 SEP 2020")
 
     def test_us06(self):
-        self.assertEqual(test_us06_div_b4_death(), "Error: Family: @F5@: US06: Died before divorce 2 FEB 2020")
+        self.assertEqual(test_us06_div_b4_death(), "Error: Family: @F6@: US06: Died before divorce 2 FEB 2020")
 
     def test_us07(self):
         self.assertEqual(test_us07_less_than_150(), "Error: Individual: @I4@: US07: Over 150 years old")
@@ -54,7 +54,7 @@ class TestUM(unittest.TestCase):
         self.assertEqual(test_us16_male_last_names(), "Error: Family: @F10@: US16 has inconsistant male last name")
 
     def test_us17(self):
-        self.assertEqual(test_us17_no_marriages_to_children(), "Error: Family @F10@: US17: Parent is married to child")
+        self.assertEqual(test_us17_no_marriages_to_children(), "Error: Family @F12@: US17: Parent is married to child")
 
     def test_us18(self):
         self.assertEqual(test_us18_siblings_should_not_marry(), "Error: Indi: @I4@: US18: Siblings can't be married")
@@ -63,7 +63,7 @@ class TestUM(unittest.TestCase):
         self.assertEqual(test_us19_first_cousins_should_not_marry(), "Error: Indi: @I1@@I1@: US19: First cousins should not marry")
 
     def test_us21(self):
-        self.assertEqual(test_us21_correct_gender_role(), "Error: Family: @F5@: US21: Wife is wrong gender")
+        self.assertEqual(test_us21_correct_gender_role(), "Error: Family: @F11@: US21: Wife is wrong gender")
 
     def test_us22(self):
         self.assertEqual(test_us22_unique_IDs(), "Error: Individual: US22: Duplicate ID number")
@@ -81,16 +81,16 @@ class TestUM(unittest.TestCase):
         self.assertEqual(test_us26_corresponding_entries(),'Error Individual:@I4@ US26 individual table and family table do not match')
 
     def test_us27(self):
-        self.assertEqual(test_us27_include_individual_ages(), "US27: Include individual ages: @I35@ 23")
+        self.assertEqual(test_us27_include_individual_ages(), "US27: Include individual ages: @I41@ 26")
 
     def test_us28(self):
-        self.assertEqual(test_us28_order_siblings_by_age(), "US28: Order siblings from oldest to youngest: @F11@ [23]")
+        self.assertEqual(test_us28_order_siblings_by_age(), "US28: Order siblings from oldest to youngest: @F12@ [23]")
 
     def test_us29(self):
-        self.assertEqual(test_us29_list_deceased(), "US29: List of all deaths in tree: ['Jay /Rana/', 'Angelina /Iannacone/', 'Dev /Rana/', 'Jonathan /Dixon/']")
+        self.assertEqual(test_us29_list_deceased(), "US29: List of all deaths in tree: ['Jay /Rana/', 'Angelina /Iannacone/', 'Jacob Davidson /Davidson/', 'Dev /Rana/', 'Jonathan /Dixon/', 'Angela /Rana/', 'Alex /Johnson/']")
 
     def test_us32(self):
-        self.assertEqual(test_us32_list_multiple_births(), "US32: List of all multiple births: [{'Siblings': ['@I21@: Lisa /Iannacone/', '@I22@: Matt /Iannacone/'], 'Fam ID': '@F3@', 'Birth': '10 FEB 1995'}, {'Siblings': ['@I8@: Angelina /Iannacone/', '@I13@: Monica /Iannacone/', '@I15@: Junior /Iannacone/', '@I16@: Dominic /Iannacone/', '@I17@: John /Iannacone/', '@I18@: Jane /Iannacone/'], 'Fam ID': '@F3@', 'Birth': '10 MAY 1989'}, {'Siblings': ['@I24@: Luke /Iannacone/', '@I25@: Peter /Iannacone/', '@I27@: Danielle /Iannacone/'], 'Fam ID': '@F3@', 'Birth': '10 AUG 1996'}, {'Siblings': ['@I19@: Joe /Iannacone/', '@I20@: Mary /Iannacone/'], 'Fam ID': '@F3@', 'Birth': '10 MAY 1990'}, {'Siblings': ['@I28@: Jane /Fairfield/', '@I29@: Jane /Fairfield/'], 'Fam ID': '@F6@', 'Birth': '11 APR 2015'}]")
+        self.assertEqual(test_us32_list_multiple_births(), "US32: List of all multiple births: [{'Siblings': ['@I21@: Lisa /Iannacone/', '@I22@: Matt /Iannacone/'], 'Fam ID': '@F3@', 'Birth': '10 FEB 1995'}, {'Siblings': ['@I8@: Angelina /Iannacone/', '@I11@: Monica /Iannacone/', '@I15@: Junior /Iannacone/', '@I16@: Dominic /Iannacone/', '@I17@: John /Iannacone/', '@I18@: Jane /Iannacone/'], 'Fam ID': '@F3@', 'Birth': '10 MAY 1989'}, {'Siblings': ['@I24@: Luke /Iannacone/', '@I27@: Danielle /Iannacone/'], 'Fam ID': '@F3@', 'Birth': '10 AUG 1996'}, {'Siblings': ['@I19@: Joe /Iannacone/', '@I20@: Mary /Iannacone/'], 'Fam ID': '@F3@', 'Birth': '10 MAY 1990'}, {'Siblings': ['@I28@: Jane /Fairfield/', '@I29@: Jane /Fairfield/'], 'Fam ID': '@F5@', 'Birth': '11 APR 2015'}, {'Siblings': ['@I35@: Peter /Iannacone2/', '@I35@: Peter /Iannacone2/', '@I35@: Peter /Iannacone2/'], 'Fam ID': '@F12@', 'Birth': '10 AUG 1996'}]")
 
     def test_us33(self):
         self.assertEqual(test_us33_list_orphans(), "US33: List of all orphans: ['John /Davidson/', 'Bob /Johnson/']")
@@ -102,7 +102,7 @@ class TestUM(unittest.TestCase):
         self.assertEqual(test_us36_list_recent_deaths(), "US36: List of all individuals who died within the last 30 days: ['Jonathan /Dixon/']")
 
     def test_us38(self):
-        self.assertEqual(test_us38_list_upcoming_birthdays(), "US38: List of all individuals whose birthdays are in the next 30 days: ['Dominic /Iannacone/', 'Monica /Iannacone/', 'Junior /Iannacone/', 'Dominic /Iannacone/', 'John /Iannacone/', 'Jane /Iannacone/', 'Joe /Iannacone/', 'Mary /Iannacone/', 'John /Smith/']")
+        self.assertEqual(test_us38_list_upcoming_birthdays(), "US38: List of all individuals whose birthdays are in the next 30 days: ['Monica /Iannacone/', 'Dominic /Iannacone/', 'Junior /Iannacone/', 'Dominic /Iannacone/', 'John /Iannacone/', 'Jane /Iannacone/', 'Joe /Iannacone/', 'Mary /Iannacone/', 'John /Smith/']")
 
 
 
